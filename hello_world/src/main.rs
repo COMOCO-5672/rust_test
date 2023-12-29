f9894c8a8d252710525a631261a13bd5e9a0cb25
comoco-5672 <775324623@qq.com>
2023-12-29 23:20:40 +0800

    添加变量

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+
+fn hello_world() {
+    println!("hahahhaha");
+}
+
 fn main() {
     println!("Hello, world!");
+    hello_world();
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("无法");
+
+    println!("你猜测的数{}", guess);
+
 }
